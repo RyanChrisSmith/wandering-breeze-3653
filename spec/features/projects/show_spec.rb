d05e0a2643a5c1d660c3a18c6df445bdc727b955
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-04 15:23:01 -0600

    projects show page path all set up

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
-Rspec.describe 'Projects show page' do 
+RSpec.describe 'Projects show page' do
   describe 'As a visitor, When I visit a projects show page ("/projects/:id"),' do 
     before :each do
-      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
-      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+      ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
     end
     it 'I see that projects name and material as well as the theme of the challenge' do 
-      visit "/projects"
+      visit "/projects/#{@news_chic.id}"
 
+      expect(current_path).to eq("/projects/#{@news_chic.id}")
+      expect(page).to have_content("News Chic")
+      expect(page).to have_content("Material: #{@news_chic.material}")
+      expect(page).to have_content("Challenge Theme: Recycled Material")
     end
   end
 end
@@ -30,6 +34,6 @@ end
 
 
 
-(e.g.    Litfit
-    Material: Lamp Shade
-  Challenge Theme: Apartment Furnishings)
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings)
